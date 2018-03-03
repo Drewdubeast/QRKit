036a6fbaaca882253c78df861c29e7c10a05b577
Drew Wilken <wilk0001@stthomas.edu>
2018-03-03 13:15:29 -0600

    Started framework refactoring

--- QRKit/QRRead.swift
+++ QRKit/QRRead.swift
@@ -10,88 +10,128 @@ import Foundation
 import UIKit
 import AVFoundation
 
-class QRRead: UIViewController {
+public final class QRRead: UIViewController {
     
     var session = AVCaptureSession()
+    var codeViewFrame: UIView?
+    var captureDevice: AVCaptureDevice?
     
-    var previewLayer: AVCaptureVideoPreviewLayer?
-    var contactViewFrame: UIView?
+    var codeString: String?
     
-    @IBOutlet weak var bottomScannerBar: UILabel!
-    @IBOutlet weak var topBar: UILabel!
+    var discoverySession: AVCaptureDevice.DiscoverySession? {
+        return AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera,.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .unspecified)
+    }
+    var metaDataCapture = AVCaptureMetadataOutput()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    //initializer for the reading framework
+    public init() {
+        super.init(nibName: nil, bundle: nil)
         
-        //create a discovery session to find the device
-        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera,.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back)
+        self.codeString = nil
         
-        print(discoverySession.devices)
+        //get everything running
+        createCameraView()
+        configure()
+    }
+    //required init
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //return the preview layer for the camera
+    public func getPreviewLayer(session: AVCaptureSession) -> AVCaptureVideoPreviewLayer {
+        let previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+        previewLayer.frame = self.view.bounds
         
-        guard let device = discoverySession.devices.first else {
-            print("Couldn't find rear-facing camera.")
-            return
+        return previewLayer
+    }
+    
+    public func createCameraView() {
+        self.view.layer.addSublayer(getPreviewLayer(session: self.session))
+    }
+    
+    //returns a device!
+    func getDevice() -> AVCaptureDevice? {
+        guard let discoverySession = self.discoverySession else {
+            return nil
         }
-        
-        //try creating a new input from the capture device
+        for device in discoverySession.devices {
+            if (device.position == .back) {
+                captureDevice = device
+                return device
+            }
+        }
+        return nil
+    }
+    
+    func configure() {
         do {
-            let input = try AVCaptureDeviceInput(device: device)
+            guard let captureDevice = getDevice() else {
+                return
+            }
             
+            let input = try AVCaptureDeviceInput(device: captureDevice)
             //add the input to the session
-            session.addInput(input)
+            
+            //session.addInput(input)
             
             //add the output for the metadata to the session
-            let metaDataCapture = AVCaptureMetadataOutput()
-            session.addOutput(metaDataCapture)
-            
-            //set the delegate to self because following the delegate protocol here
+            let output = AVCaptureMetadataOutput()
             metaDataCapture.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             metaDataCapture.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
             
+            //if all is good, then add inputs and outputs and commit
+            if (self.session.canAddInput(input) && self.session.canAddOutput(metaDataCapture)) {
+                self.session.addInput(input)
+                self.session.addOutput(output)
+                self.session.commitConfiguration()
+                self.session.startRunning()
+            }
         } catch {
             print(error)
             return
         }
-        
-        //create the preview layer and add it as a subview
-        previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        previewLayer?.frame = view.layer.bounds
-        view.layer.addSublayer(previewLayer!)
-        
-        session.startRunning()
-        
-        view.bringSubview(toFront: bottomScannerBar)
-        
-        contactViewFrame = UIView()
-    
     }
-    
-    override func didReceiveMemoryWarning() {
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //try creating a new input from the capture device
+        
+        codeViewFrame = UIView()
+        
+        if let codeViewFrame = codeViewFrame {
+            codeViewFrame.layer.borderColor = UIColor.green.cgColor
+            codeViewFrame.layer.borderWidth = 4
+            view.addSubview(codeViewFrame)
+            view.bringSubview(toFront: codeViewFrame)
+        }
+    }
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
 
 extension QRRead: AVCaptureMetadataOutputObjectsDelegate {
-    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+    public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         
         if (metadataObjects.count == 0) {
             print("No metadata objects found")
-            contactViewFrame?.frame = CGRect.zero
             return
         }
         
         let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
         
-        
         if metadataObj.type == AVMetadataObject.ObjectType.qr {
             // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
-            let barCodeObject = previewLayer?.transformedMetadataObject(for: metadataObj)
-            contactViewFrame?.isHidden = false
-            contactViewFrame?.frame = barCodeObject!.bounds
+            let barCodeObject = getPreviewLayer(session: self.session).transformedMetadataObject(for: metadataObj)
+            codeViewFrame?.isHidden = false
+            codeViewFrame?.frame = barCodeObject!.bounds
             
+            if let codeString = metadataObj.stringValue {
+                print(codeString)
+                self.codeString = codeString
+            }
         }
     }
 }
-
