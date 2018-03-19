0c845da9dfeaa0f9d416e03efd2709cf49aa5eca
Drew Wilken <wilk0001@stthomas.edu>
2018-03-18 20:11:37 -0500

    Started on QR reader part of framework

---
+++ QRKit/QRReaderController.swift
@@ -0,0 +1,90 @@
+//
+//  QRReaderController.swift
+//  QRKit
+//
+//  Created by Drew Wilken on 3/18/18.
+//  Copyright © 2018 Drew Wilken. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+public final class QRReaderController: UIViewController {
+    
+    var session = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    var discoverySession: AVCaptureDevice.DiscoverySession? {
+        return AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera,.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back)
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        setupCamera()
+    }
+    
+    func setupCamera() {
+        guard let device = getDevice(with: .back) else {
+            return
+        }
+        
+        //try creating a new input from the capture device
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            
+            //add the input to the session
+            session.addInput(input)
+            
+            //add the output for the metadata to the session
+            let metaDataCapture = AVCaptureMetadataOutput()
+            session.addOutput(metaDataCapture)
+            
+            //set the delegate to self because following the delegate protocol here
+            metaDataCapture.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataCapture.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+        } catch {
+            print(error)
+            return
+        }
+        
+        //create the preview layer and add it as a subview
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(previewLayer!)
+        
+        session.startRunning()
+    }
+    
+    //Get device
+    func getDevice(with position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        guard let discoverySession = self.discoverySession else {
+            return nil
+        }
+        for device in discoverySession.devices {
+            if(device.position == position) {
+                return device
+            }
+        }
+        return nil
+    }
+}
+
+// MARK: Metadata Processing
+
+extension QRReaderController: AVCaptureMetadataOutputObjectsDelegate {
+    public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        if (metadataObjects.count == 0) {
+            print("No metadata objects found")
+            return
+        }
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            if let metadataString = metadataObj.stringValue {
+               print(metadataString)
+            }
+        }
+    }
+}
+
