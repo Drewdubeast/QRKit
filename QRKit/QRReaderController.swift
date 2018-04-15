600e592b97ca2428834062e7ecaa6f0dee8c249a
Drew Wilken <wilk0001@stthomas.edu>
2018-04-14 21:21:26 -0500

    Started more code cleanup and position changing

--- QRKit/QRReaderController.swift
+++ QRKit/QRReaderController.swift
@@ -22,66 +22,95 @@ public protocol QRReaderDelegate {
 
 public final class QRReaderController: UIViewController {
     
-    private var session = AVCaptureSession()
-    private var previewLayer: AVCaptureVideoPreviewLayer?
-    private var discoverySession: AVCaptureDevice.DiscoverySession? {
-        return AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera,.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back)
-    }
+    //create inputs and outputs
+    var input: AVCaptureDeviceInput?
+    let metaDataCaptureOutput = AVCaptureMetadataOutput()
     
     //Delegate - Should be the app that
     public var delegate: QRReaderDelegate?
-    public var Position = CameraPosition.back
+    
+    fileprivate var session = AVCaptureSession()
+    fileprivate var previewLayer: AVCaptureVideoPreviewLayer?
+    fileprivate var discoverySession: AVCaptureDevice.DiscoverySession? {
+        return AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera,.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back)
+    }
+    
+    var position: CameraPosition = .back {
+        
+        //if the camera's position was changed, then stop it and update it
+        didSet {
+            if self.session.isRunning {
+                self.session.stopRunning()
+                updateCamera()
+            }
+        }
+    }
     
     override public func viewDidLoad() {
         super.viewDidLoad()
-        setupCamera()
-        //delegate?.processQR("Woah there guy, can't find me ;)")
+        updateCamera()
+        delegate?.processQR("Woah there guy, can't find me ;)")
     }
     
     public init() {
         super.init(nibName: nil, bundle: nil)
-        
-        //delegate will be set to the controller/class that will handle the QR
-        //findings
-        self.delegate = delegate
     }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupCamera() {
-        guard let device = getDevice(with: .back) else {
+    private func updateCamera() {
+        
+        //reset inputs
+        if let currentInput = self.input {
+            self.session.removeInput(currentInput)
+        }
+        //gets device based on position input
+        guard let device = getDevice(with: position == .back ? AVCaptureDevice.Position.back : AVCaptureDevice.Position.front) else {
             return
         }
         
         //try creating a new input from the capture device
         do {
+            //create inputs and outputs
             let input = try AVCaptureDeviceInput(device: device)
             
-            //add the input to the session
-            session.addInput(input)
-            
-            //add the output for the metadata to the session
-            let metaDataCapture = AVCaptureMetadataOutput()
-            session.addOutput(metaDataCapture)
+            if(session.canAddInput(input) && session.canAddOutput(metaDataCaptureOutput)) {
+                //add the input to the session
+                session.addInput(input)
+                
+                //add the output for the metadata to the session
+                session.addOutput(metaDataCaptureOutput)
+                
+                session.commitConfiguration()
+                session.startRunning()
+            }
             
             //set the delegate to self because following the delegate protocol here
-            metaDataCapture.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            metaDataCapture.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            metaDataCaptureOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataCaptureOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
             
         } catch {
             print(error)
             return
         }
         
+        guard let previewLayer = getPreviewLayer() else {
+            return
+        }
+        view.layer.addSublayer(previewLayer)
+    }
+    
+    //gets the preview layer for the QR reader
+    func getPreviewLayer() -> AVCaptureVideoPreviewLayer? {
+        
         //create the preview layer and add it as a subview
-        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
         previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
         previewLayer?.frame = view.layer.bounds
-        view.layer.addSublayer(previewLayer!)
         
-        session.startRunning()
+        return previewLayer
     }
     
     //Get device
