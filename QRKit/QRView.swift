08ef9ad6bc03db5d1345f2355e6d2120d3800c43
Drew Wilken <wilk0001@stthomas.edu>
2018-04-25 23:08:20 -0500

    recolor function in progress

--- QRKit/QRView.swift
+++ QRKit/QRView.swift
@@ -12,36 +12,64 @@ import UIKit
 public class QRView: UIImageView {
 
     var string: String?
-
+    
     public override init(frame: CGRect) {
         // For use in code
         super.init(frame: frame)
-        setupQR(string: "")
+        setupQR(string: "", color: .red)
     }
     
     public required init?(coder aDecoder: NSCoder) {
         // For use in Interface Builder
         super.init(coder: aDecoder)
-        setupQR(string: "")
+        setupQR(string: "", color: .red)
     }
 
-    open func setupQR(string: String) {
-        if let qrImage = generateQRCode(for: string, view: self) {
+    open func setupQR(string: String, color: UIColor) {
+        if let qrImage = generateQRCode(for: string, view: self, for: color) {
             self.image = qrImage
         }
     }
     
-    private func generateQRCode(for string: String, view: UIView) -> UIImage? {
+    open func recolor(with color: UIColor) {
+        guard let image = self.image else {
+            return
+        }
+        guard let colorFilter = CIFilter(name: "CIFalseColor") else {
+            return
+        }
+        colorFilter.setValue(image.ciImage, forKey: "inputImage")
+        colorFilter.setValue(CIColor(red: 1, green: 1, blue: 1), forKey: "inputColor1")
+        colorFilter.setValue(CIColor(cgColor: color.cgColor), forKey: "inputColor0")
+        guard let coloredImage = colorFilter.outputImage else {
+            return
+        }
+        self.image = UIImage(ciImage: coloredImage)
+    }
+    
+    private func generateQRCode(for string: String, view: UIView, for color: UIColor) -> UIImage? {
         
         let data = string.data(using: String.Encoding.ascii)
         
         if let filter = CIFilter(name: "CIQRCodeGenerator") {
             
+            guard let colorFilter = CIFilter(name: "CIFalseColor") else {
+                return nil
+            }
+
             //set the data to the contact data
             filter.setValue(data, forKey: "inputMessage")
             filter.setValue("L", forKey: "inputCorrectionLevel")
+
+            colorFilter.setValue(filter.outputImage, forKey: "inputImage")
+            colorFilter.setValue(CIColor(red: 1, green: 1, blue: 1), forKey: "inputColor1")
+            colorFilter.setValue(CIColor(cgColor: color.cgColor), forKey: "inputColor0")
+            guard let codeImage = colorFilter.outputImage
+                else {
+                    return nil
+            }
             
-            guard let codeImage = filter.outputImage else { return nil }
+            //guard let codeImage = filter.outputImage else { return nil }
             
             //size of the contact code image
             let scaleX = view.frame.size.width / codeImage.extent.size.width
@@ -51,7 +79,7 @@ public class QRView: UIImageView {
             let transform = CGAffineTransform(scaleX: scaleX, y: scaleY)
             
             //return the image
-            if let output = filter.outputImage?.transformed(by: transform) {
+            if let output = colorFilter.outputImage?.transformed(by: transform) {
                 return UIImage(ciImage: output)
             }
         }
